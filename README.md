adfa0cd08c47a88203406bc9b23687e15d40dad9
oryon-dominik <oryon@soila.de>
2019-03-18 09:22:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# cliTube
+
+A simple CLI Interface to play Youtube-Videos from Windows-Powershell
+
+## Installation
+
+1. Your file-extensions `.py` have to be associated with Python (**>= 3.6**). If you used the Windows-Installer, that should already be the case (else look that up in the [official docs](https://docs.python.org/3/faq/windows.html#how-do-i-make-python-scripts-executable)).
+
+2. A valid installation of [VLC media player](http://www.videolan.org/vlc/) in your path-environment is required.
+
+3. If you already have a GOOGLE-DEVELOPER-API-KEY you may skip this step. Otherwise you need to [register your own API-KEY](https://developers.google.com/youtube/android/player/register) to gain Access to the required YouTube Data API.
+
+4. Have the google-api-client for Python installed.
+
+    ```shell
+    pip install google-api-python-client
+    ```
+
+5. Place cliTube.py in a directory of your choice.
+6. Create a file named `secret.py` in the same directory and enter the API-Key generated above.
+
+    ```secret.py
+    DEVELOPER_KEY = '<your secret GOOGLE-API key>'
+    ```
+
+7. Add the directory of your choice to the PATH environment variable.
+    Click [here](https://www.architectryan.com/2018/03/17/add-to-the-path-on-windows-10/) for a tutorial on how-to modify your windows-10 PATH accordingly or modify and execute the powershell-commands below for your needs.
+
+    ```powershell
+    $user_env = [Environment]::GetEnvironmentVariable("PATH", "User")
+    $cliTube_path = "C:\Users\<username>\Documents\Scripts\cliTube"
+    [Environment]::SetEnvironmentVariable("PATH", "$user_env;$cliTube_path", "User")
+    ```
+
+    Alternatively you could just set an Alias.
+
+    ```powershell
+    Set-Alias -Name tube -Value <your_path>/cliTube.py -Description "Plays Youtube Search-Results"
+    ```
+
+## Usage
+
+Type `cliTube.py <The Video-Title you want to play>`
+
+If there are several matches, cliTube will randomly select which video to play. If you don't like this "feature" modify the code for your needs accordingly. (You could set the probabilities in the function `choose` to `probabilities = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]` to always play the first match)
+
+Have fun tubing!
---
+++ cliTube.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+'''
+cliTube plays Internet-Music from CLI on Windows with preinstalled VLC
+It builds Tube-URLS from artist & title arguments
+'''
+
+
+__version__ = '0.1.1' # first local release
+__author__ = 'oryon/dominik'
+__date__ = 'November 28, 2018'
+__updated__ = 'December 3, 2018'
+
+
+import json
+import numpy as np
+import subprocess
+
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from secret import DEVELOPER_KEY
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+
+def stringify(args):
+    return ' '.join(args.search)
+
+
+def get_SearchResults_from_Youtube(search):
+    """ returns actual data of results from SearchString """
+
+    youtube = build(
+        YOUTUBE_API_SERVICE_NAME,
+        YOUTUBE_API_VERSION,
+        developerKey=DEVELOPER_KEY
+    )
+
+    search_response = youtube.search().list(
+        q=search,
+        part='id,snippet',
+        maxResults=10
+    ).execute()
+
+    return search_response
+
+
+def choose(results):
+    """ chooses the video randomly """
+    probabilities = [.3, .25, .2, .1, .05, .025, .025, .025, .0125, .0125]
+    hits, videos = {}, []
+
+    for match in results['items']:
+        ident = match['id']['videoId']
+        title = match['snippet']['title']
+        hits[f'{ident}'] = title
+
+    # print(json.dumps(hits, indent=4))  # these are the Videos concerned
+
+    for hit in hits:
+        videoURL = f'https://www.youtube.com/watch?v={hit}'
+        videos.append(videoURL)
+
+    if videos:
+        if len(videos) >= 10:
+            choice = np.random.choice(videos, p=probabilities)
+        else:
+            choice = np.random.choice(videos)
+    else:
+        print('No results for searchTerm')
+        choice = ''
+
+    return choice
+
+
+def play(url):
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.wShowWindow = 4  # this sends the window into the background on windows
+    subprocess.Popen(["vlc", f"{url}"], startupinfo=startupinfo)
+
+
+# handling the arguments
+parser = ArgumentParser(
+    description=__doc__, prog='cliTube',
+    epilog='Have fun tubing!',
+    formatter_class=RawTextHelpFormatter,
+    )
+
+parser.add_argument('--version', action='version', version=__version__)
+parser.add_argument('search', metavar='Searchterm', help='the searchstring (Artist & Title) you are looking for', nargs='*')
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    if not args.search:
+        parser.print_help()
+
+    else:
+        search = stringify(args)
+        results = get_SearchResults_from_Youtube(search)
+        match = choose(results)
+        if match:
+            play(match)
